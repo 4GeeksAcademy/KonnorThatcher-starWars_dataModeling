f78cda0981598cc8336ca17361e80e24ea180572
KonnorThatcher <131403452+KonnorThatcher@users.noreply.github.com>
2023-08-01 01:03:26 +0000

    Added some tables with some stuff in it

--- src/models.py
+++ src/models.py
@@ -1,32 +1,70 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False)
+    password = Column(String(30), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Character(Base):
+    __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(100))
+    gender = Column(String(10))
+    birth_year = Column(String(20))
+    species = Column(String(100))
+    eye_color = Column(String(20))
+    height = Column(Integer)
+    weight = Column(Integer)
+    homeworld = Column(String(200))
+    created = Column(DateTime)
+    edited = Column(DateTime)
 
-    def to_dict(self):
-        return {}
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    terrain = Column(String(100))
+    climate = Column(String(100))
+    diameter = Column(Integer)
+    hours_in_day = Column(Integer)
+    days_in_year = Column(Integer)
+    population = Column(Integer)
+    created = Column(DateTime)
+    edited = Column(DateTime)
+
+class Starship(Base):
+    __tablename__ = 'starship'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    model = Column(String(100))
+    starship_class = Column(String(100))
+    length = Column(Integer)
+    cargo_capacity = Column(Integer)
+    speed_in_mglt = Column(Integer)
+    crew_members = Column(Integer)
+    passenger_capacity = Column(Integer)
+    created = Column(DateTime)
+    edited = Column(DateTime)
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    character_id = Column(Integer, ForeignKey('character.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    starship_id = Column(Integer,ForeignKey('starship.id'))
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
